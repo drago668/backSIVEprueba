1453468fbb913df8b43933cd6313e2faca453071
Daniel Esteban Rivera Gomez <dragoartworks@gmail.com>
2025-10-26 15:57:01 -0500

    Agregando la url de carpeta Media

--- SIVEBACK/urls.py
+++ SIVEBACK/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from knox import views as knox_views
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
@@ -25,5 +27,5 @@ urlpatterns = [
     path('api/',include('api.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
