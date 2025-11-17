518b9ae0b41b0f30f80489d5a0adfd91511c1c78
HarumiNULL <165682863+HarumiNULL@users.noreply.github.com>
2025-11-16 22:57:38 -0500

    pqueño cambio en el serializer users

--- api/serializers/users_serializers.py
+++ api/serializers/users_serializers.py
@@ -5,5 +5,5 @@ class UsersSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
         fields =['id','first_name','last_name','email','role','state','is_verified_owner']
-        read_only_fields =['id', 'is_verified_owner']
+        read_only_fields =['id']
     
